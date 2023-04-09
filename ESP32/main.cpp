e0c68775ef912b7797fcb6ae1c86e8ee1bcda690
Joseph <nguyenxuankien3132004@gmail.com>
2023-04-09 19:47:13 +1000

    Update the handler for WEBSOCKET_EVENT_DATA
    Use a switch structure instead of else_if.
    The audio task is now controlled by the uno_command_client

--- ESP32/main.cpp
+++ ESP32/main.cpp
@@ -47,20 +47,22 @@ uint8_t uno_command_buffer[10];
 // This connection is for constant streaming of audio
 const esp_websocket_client_config_t audio_client_conf = {
   .uri = "ws://192.168.1.24",
-  .port = 4000, 
+  .port = 4001, // This is the server port where our client is connected
   .disable_auto_reconnect = false,
+ // .headers = "identifier: audio client",
 };
 // This connection is for receiving command from network client
 const esp_websocket_client_config_t uno_command_client_conf = {
     .uri = "ws://192.168.1.24",
-    .port = 4001,
+    .port = 4000,
     .disable_auto_reconnect = false,
+  //  .headers = "identifier: Command client",
 };
 
 // The number version of command
 std::unordered_map<std::string, uint8_t> command = {
     {"w", 1}, {"s", 2}, {"wa", 3} , {"wd", 4}, {"sa", 5}, {"sd", 6}, {"cw", 7}, {"cs", 8}, {"cd", 9}, {"ca", 10},
-    {"i", 11}, {"o", 12}, {"aw", 3}, {"dw", 4}, {"as", 5}, {"ds", 6}, {"stop", 0}, {"q", 20}, {"conau", 1}
+    {"i", 11}, {"o", 12}, {"aw", 3}, {"dw", 4}, {"as", 5}, {"ds", 6}, {"stop", 0}, {"q", 20}, {"conau", 14}, {"stopau", 13}
 };  
 
 void servo_task(void* param) {
@@ -105,33 +107,36 @@ void read_distance_task(void* param) {
     TickType_t last_wake_time;
     last_wake_time = xTaskGetTickCount();
     uint8_t distance_buffer[2];
-    std::string distance_string = "";
-    
     while (true) {
         if (esp_websocket_client_is_connected(uno_command_client)) { 
         received_data_from_uno(distance_buffer, UNO, 2);
-        //ESP_LOGI("Distance", "Front %d%d", distance_buffer[0], distance_buffer[1]);
-        //esp_websocket_client_send_bin(uno_command_client, (const char*) distance_buffer[0], 100);
-        distance_string = std::to_string(distance_buffer[0]);
+        std::string distance_string = "di";
+        distance_string.append(std::to_string(distance_buffer[0]));
         distance_string.append("b");
         distance_string.append(std::to_string(distance_buffer[1]) + "f");
         esp_websocket_client_send_text(uno_command_client, distance_string.c_str(), distance_string.size(), 1000 / portTICK_PERIOD_MS);
-
         }
+        // this function will trigger in a 500ms interval
+        //
         xTaskDelayUntil(&last_wake_time, 500 / portTICK_PERIOD_MS);
     }
 }
 
 void audio_task(void* param) {
+    
     while(true) {
-        stream_to_server(audio_client);
-        // vTaskDelay ?????
+        if (esp_websocket_client_is_connected(audio_client)) {
+            stream_to_server(audio_client);
+            // vTaskDelay ?????
+        }
+        //vTaskDelay(200/portTICK_PERIOD_MS);
     }
 }
 
 // Event handler for this system
 // Each received message will have a trailing character, sorting the command into its correct category
 void event_handler(void* arg, esp_event_base_t base, int32_t event_id, void* event_data) {
+    // Arg is the parameter passed to the handler when we register a new event
     if (base == WIFI_EVENT) {
         if (event_id == WIFI_EVENT_STA_START) {
             esp_wifi_connect();
@@ -170,45 +175,45 @@ void event_handler(void* arg, esp_event_base_t base, int32_t event_id, void* eve
      else if (base == WEBSOCKET_EVENTS) {
         if (event_id == WEBSOCKET_EVENT_DATA && (char*)arg == (char*) 'c') {
             esp_websocket_event_data_t* received_command = (esp_websocket_event_data_t*) event_data;
-            //memcpy(uno_command_buffer, received_command->data_ptr, received_command->data_len);
             std::string temp (received_command->data_ptr, received_command->data_len);
-            //ESP_LOGI("WEB SOCKET", "Received=%.*s", received_command->data_len, received_command->data_ptr);
-
-            if (temp[temp.size() - 1] == 'c') {
-                temp.pop_back();
-                uint8_t a = command[temp] & 0xFF;
+            // The UNO command client handler will orchestrate the control flow of the chip
+            switch (temp[temp.size() - 1]) {
+                case 'c': {
+                    temp.pop_back();
+                    uint8_t a = command[temp] & 0xFF;
                 
-                if (a != 7 && a != 8 && a != 9 && a != 10) {
-                    if (a == 0) {
+                    if (a != 7 && a != 8 && a != 9 && a != 10) {
+                        if (a == 0) {
+                            cam_axis_command = a;
+                        }
+                    send_command_to_uno(a, UNO, 1); // Remember to change the address back to Uno
+                    }
+
+                    else if (a == 7 || a == 8 || a == 9 || a == 10) {
                         cam_axis_command = a;
                     }
-                send_command_to_uno(a, NANO, 1); // Remember to change the address back to Uno
+            
+                break;
                 }
-
-                else if (a == 7 || a == 8 || a == 9 || a == 10) {
-                    cam_axis_command = a;
+                case 'l': {
+                    // Update LCD display
+                    temp.pop_back();
+                break;
                 }
-            }
-
-            else if (temp[temp.size() - 1] == 'l') {
-                // Update LCD display
-                temp.pop_back();
-                
-            }
-        }
-
-        else if (event_id == WEBSOCKET_EVENT_DATA && (char*)arg == (char*) 'a') {
-            esp_websocket_event_data_t* received_command = (esp_websocket_event_data_t*) event_data;
-            std::string temp (received_command->data_ptr, received_command->data_len);
-            if ((command[temp] & 0xFF) == 0) {
-                vTaskSuspend(audio_task_handle);
-            }
-            else if ((command[temp] & 0xFF) == 1) {
-                vTaskResume(audio_task_handle);
+                case 'u': {
+                    if ((command[temp] & 0xFF) == 13) {
+                        vTaskSuspend(audio_task_handle);
+                    }
+                    else if ((command[temp] & 0xFF) == 14) {
+                        vTaskResume(audio_task_handle);
+                    }
+                break;    
+                }
             }
         }
 
         else if (event_id == WEBSOCKET_EVENT_DISCONNECTED) {
+            ESP_LOGI("Web", "client disconnected");
             send_command_to_uno((0 & 0xFF), UNO, 1);
         }
      }
@@ -225,8 +230,6 @@ extern "C" void app_main() {
     }
     ESP_ERROR_CHECK(ret);
 
-    // camera_axis_sem = xSemaphoreCreateMutex();
-
     // Init GPIO
     init_gpio();
     ESP_LOGI("GPIO", "GPIO INIT DONE");
@@ -236,13 +239,18 @@ extern "C" void app_main() {
     ESP_LOGI("I2C", "Clear Bus Done");
     init_i2c();
     ESP_LOGI("I2C", " INIT DONE");
-    LCD_init(LCD, 0, 0);
+    
+    //Init LCD.
+    LCD_init(LCD, 16, 2);
     ESP_LOGI("LCD", "INIT DONE");
     LCD_home();
     LCD_clearScreen();
     LCD_writeStr("Hello humanity");
     LCD_setCursor(0, 1);
     LCD_writeStr("my name is Seph");
+
+    //initializeTFT();
+    
 
     // Init Servo. If no pulse is detected, try changing group id in servo config
     camera_axis_init(CAM_X, CAM_Y, CAM_X_CHANNEL, CAM_Y_CHANNEL);
@@ -257,6 +265,9 @@ extern "C" void app_main() {
     xTaskCreatePinnedToCore(servo_task, "Camera x y", 1024, NULL, 2, &camera_task, 1);
     ESP_LOGI("Servo", "Servo task created");
 
+    xTaskCreatePinnedToCore(audio_task, "audio task", 2048, NULL, 1, &audio_task_handle, 0);
+    ESP_LOGI("Audio", "Audio started");
+
     // Init I2S
     init_i2s();
     ESP_LOGI("I2S", " INIT DONE");
@@ -265,12 +276,12 @@ extern "C" void app_main() {
     audio_client = esp_websocket_client_init(&audio_client_conf);
     uno_command_client = esp_websocket_client_init(&uno_command_client_conf);
     
+    // WARNING: I feel like we cannot differentiate between messages using the handler arg
+
     esp_websocket_register_events(audio_client, WEBSOCKET_EVENT_ANY, event_handler, (char*) 'a');
     esp_websocket_register_events(uno_command_client, WEBSOCKET_EVENT_ANY, event_handler, (char*) 'c');
 
-    esp_websocket_client_start(audio_client);
     esp_websocket_client_start(uno_command_client);
+    esp_websocket_client_start(audio_client);
 
-    xTaskCreatePinnedToCore(audio_task, "audio task", 2048, NULL, 1, &audio_task_handle, 0);
-    ESP_LOGI("Audio", "Audio started");
 }
