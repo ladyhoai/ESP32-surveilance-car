7464ac11214e9a2e268482d5b4c017f3317782e6
Joseph <nguyenxuankien3132004@gmail.com>
2023-05-03 04:47:45 +1000

    Add initialization and functions for speaker

--- ESP32/audio_handle.cpp
+++ ESP32/audio_handle.cpp
@@ -21,6 +21,7 @@ struct wave_header {
 } header_wav;
 
 void init_i2s() {
+    // Reading from INMP441 microphone
     i2s_config_t i2s_conf = {
         .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = 16000,
@@ -41,10 +42,37 @@ void init_i2s() {
         .data_in_num = I2S_SD,
     };
     i2s_set_pin(I2S_PORT, &pin_conf);
+    
+    // Outputting PCM data to the speaker
+    i2s_config_t i2s_speaker = {
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = 16000,
+        .bits_per_sample = i2s_bits_per_sample_t(16),
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 4,
+        .dma_buf_len = 512,
+        .use_apll = false,
+    };
+    i2s_driver_install(I2S_SPEAK, &i2s_speaker, 0, NULL);
+
+    i2s_pin_config_t pin_speak = {
+        .bck_io_num = I2S_CLK_SPEAK,
+        .ws_io_num = I2S_WS_SPEAK,
+        .data_out_num = I2S_SD_SPEAK,
+        .data_in_num = -1,
+    };
+    i2s_set_pin(I2S_SPEAK, &pin_speak);
 }
 
 void stream_to_server(esp_websocket_client_handle_t audio) {
     uint16_t audio_buffer[audio_buffer_size];
     i2s_read(I2S_PORT, audio_buffer, audio_buffer_size, &byte_read, 10000 / portTICK_PERIOD_MS);
     esp_websocket_client_send_bin(audio, (const char*) audio_buffer, audio_buffer_size, 10000/portTICK_PERIOD_MS);
+}
+
+void output_to_speaker(const char* data, int len) {
+    size_t byte_written = 0;
+    i2s_write(I2S_SPEAK, data, len, &byte_written, 1000 / portTICK_PERIOD_MS);
 }
