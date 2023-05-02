4323016419a645b9f3a8df980fc6a8938a9edc67
Joseph <nguyenxuankien3132004@gmail.com>
2023-05-03 04:45:03 +1000

    Add function to output audio buffer to the speaker

--- ESP32/audio_handle.h
+++ ESP32/audio_handle.h
@@ -5,10 +5,13 @@
 #include <driver/i2s.h>
 #include <car_pin.h>
 #include <esp_websocket.h>
+#include <esp_log.h>
 
 #define I2S_PORT I2S_NUM_0 
+#define I2S_SPEAK I2S_NUM_1
     
 void init_i2s();
 void stream_to_server(esp_websocket_client_handle_t audio);
+void output_to_speaker(const char* data, int len);
 
 #endif
