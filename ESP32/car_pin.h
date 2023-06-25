5a982c3e690aa92f2d8387622e2634ee78966fa3
Joseph <nguyenxuankien3132004@gmail.com>
2023-06-26 01:01:20 +1000

    Update car_pin.h
    Include pin definition for speaker enable pin

--- ESP32/car_pin.h
+++ ESP32/car_pin.h
@@ -15,14 +15,22 @@
 
 #define    HCSR501 GPIO_NUM_35
 
-#define    CAM_X   GPIO_NUM_19
-#define    CAM_Y   GPIO_NUM_18
+#define    CAM_X   GPIO_NUM_18
+#define    CAM_Y   GPIO_NUM_19
 
-#define    STEPPER_EN     GPIO_NUM_25
 #define    STEPPER_DIR    GPIO_NUM_26
 
 #define    SDA            GPIO_NUM_21
 #define    SCL            GPIO_NUM_22
+
+#define SPEAKER_EN GPIO_NUM_23
+
+#define SDA_LCD GPIO_NUM_16
+#define SCL_LCD GPIO_NUM_17
+
+#define CONFIG_TFT_DISPLAY_WIDTH 480
+#define CONFIG_TFT_DISPLAY_HEIGHT 320
+
 
 #define I2S_NETWORK_PORT  4000
 #define COMMAND_PORT      4001
